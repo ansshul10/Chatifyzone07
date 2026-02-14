646907f87440827a1fe14ea0495654228e88b541
ansshul10 <ansshul10@gmail.com>
2026-02-15 00:36:26 +0530

    Delete client directory

--- client/src/api/axios.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const axiosInstance = axios.create({
-    baseURL: 'https://chatifyzone07.onrender.com/api',
-    withCredentials: true, // Sabhi requests ke sath cookies jayengi
-});
-
-export default axiosInstance;
