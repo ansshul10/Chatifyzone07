dbb7c9a3ca33ef733a0c89254ff360a6cb44397e
ansshul10 <ansshul10@gmail.com>
2026-02-14 23:53:51 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@ const server = http.createServer(app);
 // ────────────────────────────────────────────────
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://chatifyzone07.vercel.app",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
   },
